0cf6278bbe1d07af97e9ddb9ede2a4a42a93d47b
Hitesh Tewari <hitesh.a.tewari@capgemini.com>
2023-05-12 07:36:55 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-40644",
-  url: "https://test-40644.botics.co",
-  api: "https://test-40644.botics.co/api/v1"
-}
+  "name": "test-40644",
+  "url": "https://test-40644.botics.co",
+  "api": "https://test-40644.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
